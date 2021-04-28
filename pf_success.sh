ee80152fc582374c8d5ff924c2d1a41ab95e13da
thrnz <thrnz@users.noreply.github.com>
2021-04-28 21:40:40 +1200

    Added FWD_IFACE and PF_DEST_IP env vars
    See issue #20

--- pf_success.sh
+++ pf_success.sh
@@ -5,3 +5,11 @@ if [ $FIREWALL -eq 1 ]; then
   iptables -A INPUT -p udp -i wg0 --dport "$1" -j ACCEPT
   echo "$(date): Allowing incoming traffic on port $1"
 fi
+
+# Set env var PF_DEST_IP to forward on to another address
+# eg PF_DEST_IP=192.168.1.48
+if [ -n "$PF_DEST_IP" ]; then
+  iptables -t nat -A PREROUTING -p tcp --dport "$1" -j DNAT --to-destination "$PF_DEST_IP:$1"
+  iptables -t nat -A PREROUTING -p udp --dport "$1" -j DNAT --to-destination "$PF_DEST_IP:$1"
+  echo "$(date): Forwarding incoming VPN traffic on port $1 to $PF_DEST_IP:$1"
+fi
