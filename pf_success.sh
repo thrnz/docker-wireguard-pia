9de286f286fb4763bfdb7d6e02f0d9db0ca2f47f
thrnz <thrnz@users.noreply.github.com>
2021-04-29 19:52:22 +1200

    Fixed port forwarding when using container as a gateway

--- pf_success.sh
+++ pf_success.sh
@@ -8,8 +8,10 @@ fi
 
 # Set env var PF_DEST_IP to forward on to another address
 # eg PF_DEST_IP=192.168.1.48
-if [ -n "$PF_DEST_IP" ]; then
+if [ -n "$PF_DEST_IP" ] && [ -n "$FWD_IFACE" ]; then
   iptables -t nat -A PREROUTING -p tcp --dport "$1" -j DNAT --to-destination "$PF_DEST_IP:$1"
   iptables -t nat -A PREROUTING -p udp --dport "$1" -j DNAT --to-destination "$PF_DEST_IP:$1"
+  iptables -A FORWARD -i wg0 -o "$FWD_IFACE" -p tcp -d "$PF_DEST_IP" --dport "$1" -j ACCEPT
+  iptables -A FORWARD -i wg0 -o "$FWD_IFACE" -p udp -d "$PF_DEST_IP" --dport "$1" -j ACCEPT
   echo "$(date): Forwarding incoming VPN traffic on port $1 to $PF_DEST_IP:$1"
 fi
